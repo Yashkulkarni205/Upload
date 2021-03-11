2931e21cf6a491f97f2056cabf71d14dd0a60386
Yashkulkarni205 <yashkulkarni205@gmail.com>
2021-03-11 14:30:08 +0530

    final commit

--- upload/mainapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Upload(models.Model):
-    upload_file = models.FileField(verbose_name='file',upload_to='')
-
-    def __str__(self):
-        return self.upload_file.name
